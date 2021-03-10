28e64e855fc5372d3292598fcf2db47bc0bf4713
Warren Uniewski <warren@autimo.com>
2021-03-10 10:42:19 -0800

    Add type definitions to firewall module variables

--- modules/firewall/variables.tf
+++ modules/firewall/variables.tf
@@ -1,15 +1,21 @@
 variable "alb_fqdn" {
-
+  description = "Map alb fqdns to environments"
+  type = map(object({
+    dns_name = string
+  }))
 }
 
 variable "extip" {
-
+  description = "Private IP adddress of the firewall used for the virtual IP"
+  type        = string
 }
 
 variable "extport" {
-
+  description = "Port used for the virtual IP"
+  type        = number
 }
 
 variable "identifier" {
-
+  description = "Project identifier"
+  type        = string
 }
