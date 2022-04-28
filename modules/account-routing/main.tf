b5a8c8935ca074d4199dbfc2b2428f5e8ee875be
Warren Uniewski <warren@autimo.com>
2022-04-28 15:03:46 -0700

    add cert validation

--- modules/account-routing/main.tf
+++ modules/account-routing/main.tf
@@ -63,12 +63,12 @@ resource "aws_acm_certificate_validation" "cert_validation" {
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 
-data "aws_lb_listener" "public_lb" {
-  load_balancer_arn = data.aws_lb.public_lb.arn
+data "aws_lb_listener" "public_alb" {
+  load_balancer_arn = data.aws_lb.public_alb.arn
   port              = 443
 }
 
 resource "aws_lb_listener_certificate" "cert" {
-  listener_arn    = data.aws_lb_listener.public_lb.arn
+  listener_arn    = data.aws_lb_listener.public_alb.arn
   certificate_arn = aws_acm_certificate_validation.cert_validation.certificate_arn
 }
