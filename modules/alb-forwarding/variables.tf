2bcb39fa2e8fb6cd24567c4e10a93ab0e250353e
Warren Uniewski <warren@autimo.com>
2022-05-17 16:45:29 -0700

    add parent_domain variable

--- modules/alb-forwarding/variables.tf
+++ modules/alb-forwarding/variables.tf
@@ -8,6 +8,11 @@ variable "subdomain" {
   type        = string
 }
 
+variable "parent_domain" {
+  type        = string
+  description = "The parent domain (zone) for the resources created by the module."
+}
+
 variable "target_dns_name" {
   description = "Target DNS name"
   type        = string
